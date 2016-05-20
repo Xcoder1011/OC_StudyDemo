7c9c6f2a9f298a00a0ffb8c8321c7c790fcc9299
Xcoder1011 <shangkunwu@163.com>
2016-05-20 17:41:56 +0800

    update networking

--- 断点续传/SKNetworkingDemo/SKNetworkingDemo/SKNetworking/SKNetworking.h
+++ 断点续传/SKNetworkingDemo/SKNetworkingDemo/SKNetworking/SKNetworking.h
@@ -8,6 +8,55 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  下载进度
+ *
+ *  @param bytesRead      已下载文件的大小
+ *  @param totalBytesRead 文件总大小
+ */
+typedef void(^SKDownloadProgress)(int64_t bytesRead,
+                                  int64_t totalBytesRead);
+
+/**
+ *  请求成功
+ *
+ *  @param response 请求成功返回的数据
+ */
+typedef void(^SKResponseSuccess)(id response);
+/**
+ *  请求失败
+ *
+ *  @param error 请求失败错误信息
+ */
+typedef void(^SKResponseFailure)(NSError *error);
+
+/**
+ *  所有接口返回的类型都是基类NSURLSessionTask，若要接收返回值
+ *  且处理，请转换成对应的子类类型
+ */
+typedef NSURLSessionTask SKURLSessionTask;
+
+
 @interface SKNetworking : NSObject
+
+/**
+ *  网络接口的基础URL
+ */
++ (NSString *)baseUrl;
+
+/**
+ *  下载文件
+ *
+ *  @param url       下载URL
+ *  @param cachePath 缓存路径
+ *  @param progress  下载进度
+ *  @param success   下载成功回调
+ *  @param failure   下载失败回调
+ */
++ (SKURLSessionTask *)downloadWithUrl:(NSString *)url
+                            cachePath:(NSString *)cachePath
+                             progress:(SKDownloadProgress)progress
+                              success:(SKResponseSuccess)success
+                              failure:(SKResponseFailure)failure;
 
 @end
--- 断点续传/SKNetworkingDemo/SKNetworkingDemo/SKNetworking/SKNetworking.m
+++ 断点续传/SKNetworkingDemo/SKNetworkingDemo/SKNetworking/SKNetworking.m
@@ -8,6 +8,28 @@
 
 #import "SKNetworking.h"
 
+static NSString *default_baseUrl = nil;
+
 @implementation SKNetworking
 
++ (NSString *)baseUrl{
+   return  default_baseUrl;
+}
+
++ (SKURLSessionTask *)downloadWithUrl:(NSString *)url
+                            cachePath:(NSString *)cachePath
+                             progress:(SKDownloadProgress)progress
+                              success:(SKResponseSuccess)success
+                              failure:(SKResponseFailure)failure{
+//    if (![self baseUrl]) {
+//        // nil
+//        <#statements#>
+//    }
+
+
+
+
+
+
+}
 @end
